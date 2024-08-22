f7c402f130e156e87f9bcd082339950f941f2ee6
Denilson Nascimento <denilsonn7187@gmail.com>
2024-08-22 18:33:33 -0300

    Importando Cheerio corretamente e lendo linhas tabela do ranking dos atletas

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 import axios from "axios";
+import * as cheerio from "cheerio";
 
-const url = 'https://www.procyclingstats.com/rankings.php';
+const url = 'https://www.procyclingstats.com/rankings/me/individual';
 
 async function getData() {
     try {
         const responseRanking = await axios.get(url);
-        console.log(responseRanking.data);
+        const html = responseRanking.data;
+        const $ = cheerio.load(html);
+        const table = $('table.basic');
+        console.log('Table HTML:', table.html());
     } catch (error) {
         console.error('Error retrieving the data:', error);
     };
+
 }
 
 getData();
