9b515128fdb656a7473b2b629d90826c72898449
Denilson Nascimento <denilsonn7187@gmail.com>
2024-08-22 19:20:19 -0300

    Geting the table line by line

--- index.js
+++ index.js
@@ -8,12 +8,17 @@ async function getData() {
         const responseRanking = await axios.get(url);
         const html = responseRanking.data;
         const $ = cheerio.load(html);
-        const table = $('table.basic');
-        console.log('Table HTML:', table.html());
+        const rows = $('table.basic tbody tr');
+
+        for (let i = 0; i < 11 ; i++) {
+            let specifcRow = rows.eq(i)
+            console.log(specifcRow.html())
+        }
+
     } catch (error) {
         console.error('Error retrieving the data:', error);
     };
-
 }
 
 getData();
+
