554f46fe5d7f8b32ce91f26437e105f63baac2e4
Denilson Nascimento <denilsonn7187@gmail.com>
2024-08-26 19:53:23 -0300

    configurando endpoint do ranking mundial individual

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,19 @@ import { getRaceResults,getIndividualRanking } from "./index.js";
 
 const router = Router();
 
-router.get('/', async (req,res) => {
+router.get('/cycling/raceresults', async (req,res) => {
     try{
         let resultado = await getRaceResults();
+       
+        res.json(resultado)
+    } catch(error) {
+        res.send(`Ocorreu um erro ${error}`)
+    }
+    
+})
+router.get('/cycling/individualranking', async (req,res) => {
+    try{
+        let resultado = await getIndividualRanking(20);
        
         res.json(resultado)
     } catch(error) {
